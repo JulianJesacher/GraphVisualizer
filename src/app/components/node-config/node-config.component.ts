b477da097d3fb03a9035f5fd88f3c7680c554270
Julian Jesacher <julianjesacher@gmail.com>
2022-08-07 22:47:56 +0200

    Show error messages if nodeConfig is invalid

--- src/app/components/node-config/node-config.component.ts
+++ src/app/components/node-config/node-config.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Node } from 'vis';
+import { GraphDataService } from '../../services/graph-data.service';
 
 @Component({
   selector: 'app-node-config',
@@ -8,8 +10,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class NodeConfigComponent implements OnInit {
   public nodeConfigForm: FormGroup;
+  public configErrorState: NodeConfigErrorState = NodeConfigErrorState.NONE;
+  @Input() nodeToEdit: Node = { id: -1, label: 'new' };
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private graphData: GraphDataService) {
     this.nodeConfigForm = this.fb.group({
       id: this.fb.control(null, [Validators.required]),
       label: this.fb.control(null, [Validators.required]),
@@ -18,7 +22,46 @@ export class NodeConfigComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  updateNode(){
-    console.log(this.nodeConfigForm.value);
+  updateNode() {
+    if (!this.nodeConfigForm.valid) {
+      if (this.nodeConfigForm.value.id === null) {
+        this.configErrorState = NodeConfigErrorState.ID_NOT_PROVIDED;
+      } else {
+        this.configErrorState = NodeConfigErrorState.LABEL_NOT_PROVIDED;
+      }
+      return;
+    }
+
+    const nodeConfig: Node = this.nodeConfigForm.value;
+    let idExists: boolean = false;
+    this.graphData.graphNodes.forEach((node) => {
+      console.log(
+        node.id,
+        this.nodeToEdit.id,
+        nodeConfig.id != this.nodeToEdit.id,
+        nodeConfig.id == node.id
+      );
+      //New id chosen which is already in the dataset
+      if (nodeConfig.id != this.nodeToEdit.id && nodeConfig.id == node.id) {
+        idExists = true;
+      }
+    });
+
+    if (idExists) {
+      this.configErrorState = NodeConfigErrorState.ID_EXISTS;
+      return;
+    }
+
+    this.graphData.graphNodes.add({
+      id: nodeConfig.id,
+      label: nodeConfig.label,
+    });
   }
+}
+
+enum NodeConfigErrorState {
+  NONE = '',
+  ID_EXISTS = 'A node with this id already exists',
+  ID_NOT_PROVIDED = 'No id was provided',
+  LABEL_NOT_PROVIDED = 'No label was provided',
 }
