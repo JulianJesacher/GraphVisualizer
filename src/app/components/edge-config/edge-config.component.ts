cd9d91fe29cb8ae78569eee5073de3c7c4d693d0
Julian Jesacher <julianjesacher@gmail.com>
2022-08-07 23:47:40 +0200

    Add functionality to edit edge

--- src/app/components/edge-config/edge-config.component.ts
+++ src/app/components/edge-config/edge-config.component.ts
@@ -23,6 +23,44 @@ export class EdgeConfigComponent implements OnInit {
   ngOnInit(): void {}
 
   /**
+   * Checks provided id and label and shows error if a value is missing or the given id already exists in the graph. If everything is valid, the edge is updated.
+   */
+  updateEdge() {
+    if (!this.edgeConfigForm.valid) {
+      if (this.edgeConfigForm.value.id === null) {
+        this.configErrorState = EdgeConfigErrorState.ID_NOT_PROVIDED;
+      } else {
+        this.configErrorState = EdgeConfigErrorState.LABEL_NOT_PROVIDED;
+      }
+      return;
+    }
+
+    if (!this.edgeToEdit) {
+      return;
+    }
+
+    const edgeConfig: Edge = {
+      ...this.edgeConfigForm.value,
+      from: this.edgeToEdit.from,
+      to: this.edgeToEdit.to,
+    };
+    let idExists: boolean = false;
+    this.graphData.graphNodes.forEach((edge) => {
+      //New id chosen which is already in the dataset
+      if (edgeConfig.id != this.edgeToEdit.id && edgeConfig.id == edge.id) {
+        idExists = true;
+      }
+    });
+
+    if (idExists) {
+      this.configErrorState = EdgeConfigErrorState.ID_EXISTS;
+      return;
+    }
+
+    this.graphData.graphEdges.update(edgeConfig);
+  }
+
+  /**
    * Remove respective edge, if an id was provided.
    */
   removeEdge() {
