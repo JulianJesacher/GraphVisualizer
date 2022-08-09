af3cc75bebebabed98a9924b65adb387a889ac4b
Julian Jesacher <julianjesacher@gmail.com>
2022-08-09 23:45:12 +0200

    Close configs if click outside occured

--- src/app/components/graph-component/graph-component.component.ts
+++ src/app/components/graph-component/graph-component.component.ts
@@ -1,7 +1,15 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+} from '@angular/core';
 import { ConfigService } from 'src/app/services/config.service';
 import { Edge, Network, Node } from 'vis';
 import { GraphDataService } from '../../services/graph-data.service';
+import { EdgeConfigComponent } from '../edge-config/edge-config.component';
+import { NodeConfigComponent } from '../node-config/node-config.component';
 
 @Component({
   selector: 'app-graph-component',
@@ -9,7 +17,34 @@ import { GraphDataService } from '../../services/graph-data.service';
   styleUrls: ['./graph-component.component.css'],
 })
 export class GraphComponentComponent implements AfterViewInit {
-  @ViewChild('graphContainer') graphContainer!: ElementRef;
+  @ViewChild('graphContainer') graphContainer!: ElementRef<HTMLDivElement>;
+  @ViewChild('edgeConfig') edgeConfigElement!: EdgeConfigComponent;
+  @ViewChild('nodeConfig') nodeConfigElement!: NodeConfigComponent;
+
+  private configOpenedClick : boolean = false
+
+  // HostListener to close config element if a click outside occured
+  @HostListener('document:click', ['$event'])
+  closeConfigs(event: MouseEvent) {
+    let configContainer : undefined | EdgeConfigComponent | NodeConfigComponent = undefined;
+    if(this.configService.edgeConfigVisible && !this.configService.nodeConfigVisible){
+      configContainer = this.edgeConfigElement;
+    }else if(!this.configService.edgeConfigVisible && this.configService.nodeConfigVisible){
+      configContainer = this.nodeConfigElement;
+    }
+
+    // Return if no configContainer was selected or if viewchilds are not yet initialized, or if the config is not yet displayed (click to open the config)
+    if(configContainer===undefined || this.configOpenedClick){
+      this.configOpenedClick = false;
+      return;
+    }
+
+    if(event.target instanceof Node && !this.edgeConfigElement.container.nativeElement.contains(event.target)){
+      this.configService.edgeConfigVisible$.next(false);
+      this.configService.nodeConfigVisible$.next(false);
+    }
+    this.configOpenedClick = false;
+  }
 
   constructor(
     private graphData: GraphDataService,
@@ -45,11 +80,14 @@ export class GraphComponentComponent implements AfterViewInit {
       this.configService.nodeConfigVisible$.next(true);
       this.configService.edgeConfigVisible$.next(false);
       this.configService.nodeId$.next(event.nodes[0]);
+      
     } else if (configType === ConfigTypes.EDGE) {
       this.configService.edgeConfigVisible$.next(true);
       this.configService.nodeConfigVisible$.next(false);
       this.configService.edgeId$.next(event.edges[0]);
     }
+
+    this.configOpenedClick = true;
   }
 }
 
