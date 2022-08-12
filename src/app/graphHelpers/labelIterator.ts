76f417d0c6d7a31d4c9ac778e2a75c8caa2cec21
Julian Jesacher <julianjesacher@gmail.com>
2022-08-12 22:42:32 +0200

    Implement alphabetical label iterator

--- src/app/graphHelpers/labelIterator.ts
+++ src/app/graphHelpers/labelIterator.ts
@@ -14,3 +14,27 @@ export abstract class LabelIterator<T extends string | number>
 
   protected abstract getNextElement(): T;
 }
+
+export class AlphabeticLabelIterator extends LabelIterator<string> {
+  private currentNumberBase27 = '0'; // 1 because A is the initial currentElement, and A is 1
+
+  constructor() {
+    super('A');
+  }
+
+  protected getNextElement(): string {
+    const nextNumberBase27 = (parseInt(this.currentNumberBase27, 26) + 1).toString(26);
+    this.currentNumberBase27 = nextNumberBase27;
+    return AlphabeticLabelIterator.base27ToString(nextNumberBase27);
+  }
+
+  public static base27ToString(value: string) {
+    const chars = value.split('');
+    return chars.map((digit, index) => {
+        if (index === chars.length - 1) {
+          return String.fromCharCode(65 + parseInt(digit, 26));
+        }
+        return String.fromCharCode(64 + parseInt(digit, 26)); // Allow to show A... and not skip the A and start with B...
+      }).join('');
+  }
+}
