3e59289f631b6ab8f852ccaee7caa2c520d14423
Julian Jesacher <julianjesacher@gmail.com>
2022-08-20 00:08:40 +0200

    Add buttons to change algorithm execution

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { bfsAlgorithm } from 'src/app/algorithms/bfs.algorithm';
 import { GraphDataService } from 'src/app/services/graph-data.service';
+import { Node } from 'vis';
+import { AlgorithmService } from '../../services/algorithm.service';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.css']
+  styleUrls: ['./toolbar.component.css'],
 })
 export class ToolbarComponent implements OnInit {
-
-  constructor(private graphData : GraphDataService) { }
+  constructor(private graphData: GraphDataService, private algorithmService: AlgorithmService) {}
 
   ngOnInit(): void {
+    this.algorithmService.setAlgorithm(bfsAlgorithm);
   }
 
-  addNode(){
-      this.graphData.addNode();
+  addNode() {
+    this.graphData.addNode();
   }
 
-  addEdge(){
+  addEdge() {
     this.graphData.addEdge();
+  }
+
+  previousStep() {
+    this.algorithmService.stepBackward();
+  }
+
+  nextStep() {
+    this.algorithmService.stepForward();
+  }
+
+  start() {
+    this.algorithmService.start({ startNode: this.graphData.getNodes.get(10) as unknown as Node });
   }
 }
