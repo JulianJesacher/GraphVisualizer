5a24df44af36e0a8e9de975383683f4ec96e8c56
Julian Jesacher <julianjesacher@gmail.com>
2022-08-19 23:47:32 +0200

    Implement bfs Algorithm

---
+++ src/app/algorithms/bfs.algorithm.ts
@@ -0,0 +1,33 @@
+import { DataSet, Edge, IdType, Node } from 'vis';
+import { GraphAlgorithmInput, State, GraphAlgorithm } from '../services/algorithm.service';
+import { GraphDataService } from '../services/graph-data.service';
+import { NodeColorState } from '../services/graph-painter.service';
+
+export function* bfsAlgorithm(input: GraphAlgorithmInput, graphData: GraphDataService): Iterator<State> {
+  const queue: Node[] = [input.startNode];
+  const visited: IdType[] = [];
+  const currentState: State = {
+    nodes: new DataSet<{ node: Node; color: NodeColorState }>(graphData.getNodes.map((singleNode) => {return { node: singleNode, color: NodeColorState.NONE };})),
+    edges: new DataSet<{ edge: Edge; color: NodeColorState }>(graphData.getEdges.map((singleEdge) => {return { edge: singleEdge, color: NodeColorState.NONE };})),
+  };
+
+  while (queue.length) {
+    const currentNode: Node = queue.shift() as Node;
+    if (!currentNode?.id) {
+      continue;
+    }
+    
+    const neighbourNodes = graphData.graph.getConnectedNodes(currentNode.id) as IdType[];
+    for (const singleNeighbourId of neighbourNodes) {
+      if (!visited.includes(singleNeighbourId)) {
+        visited.push(singleNeighbourId);
+
+        const singleNeighbour = graphData.getNodes.get(singleNeighbourId);
+        if (singleNeighbour) {
+          currentState.nodes.update({ node: singleNeighbour, color: NodeColorState.EDIT });
+        }
+        yield currentState;
+      }
+    }
+  }
+}
