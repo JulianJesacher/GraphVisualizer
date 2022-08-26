c2dcf28dbfae5891568deb1b52cbd3327a0d754a
Julian Jesacher <julianjesacher@gmail.com>
2022-08-27 01:00:05 +0200

    Add component to select algorithm input nodes

---
+++ src/app/components/node-selector/node-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-node-selector',
+  templateUrl: './node-selector.component.html',
+  styleUrls: ['./node-selector.component.css']
+})
+export class NodeSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
