28f28c32b9b2f169351a96298601ac8c2368ed8a
Julian Jesacher <julianjesacher@gmail.com>
2022-08-27 00:59:17 +0200

    Add prime ng to angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { GraphComponent } from './components/graph-component/graph.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { GraphElementConfigComponent } from './components/graph-element-config/graph-element-config.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NodeSelectorComponent } from './components/node-selector/node-selector.component';
+
+import { CardModule } from 'primeng/card';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GraphComponent,
-    ToolbarComponent,
-    GraphElementConfigComponent
-  ],
-  imports: [BrowserModule, ReactiveFormsModule],
+  declarations: [AppComponent, GraphComponent, ToolbarComponent, GraphElementConfigComponent, NodeSelectorComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, ReactiveFormsModule, CardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
