703ce290394fb80d63d1d191f89c0d624e05baca
Julian Jesacher <julianjesacher@gmail.com>
2022-08-27 12:04:23 +0200

    Add functionality to select nodes for algorithm execution

--- src/app/components/algorithm-initializer/algorithm-initializer.component.ts
+++ src/app/components/algorithm-initializer/algorithm-initializer.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { InitializationInformation, inputNodesInitializationInformation } from 'src/app/types/algorithm-intializer-dialog.types';
 import { AlgorithmGroup } from '../../types/algorithm.types';
+import { UpdateCurrentNodeSelectionEvent } from '../../types/algorithm-intializer-dialog.types';
 
 @Component({
   selector: 'app-algorithm-initializer',
@@ -11,42 +13,55 @@ export class AlgorithmInitializerComponent implements OnInit {
   public requiredSteps: MenuItem[] = [];
   public activeIndex: number = 1;
 
+  private _initializationInformation?: InitializationInformation[];
   private _algorithmGroup?: AlgorithmGroup;
   @Input() set algorithmGroup(newGroup: AlgorithmGroup | null) {
+    if (!newGroup) {
+      return;
+    }
+    this._algorithmGroup = newGroup;
+    this._initializationInformation = inputNodesInitializationInformation[newGroup];
+    this.updateSteps();
   }
+
+  @Output() updateCurrentNodeSelection = new EventEmitter<UpdateCurrentNodeSelectionEvent>();
 
   constructor() {}
 
-  ngOnInit(): void {
-    this.requiredSteps = [
-      {
-        label: 'Personal',
-        command: (event: any) => {
-          this.activeIndex = 0;
-          console.log({ severity: 'info', summary: 'First Step', detail: event.item.label });
-        },
-      },
-      {
-        label: 'Seat',
-        command: (event: any) => {
-          this.activeIndex = 1;
-          console.log({ severity: 'info', summary: 'Seat Selection', detail: event.item.label });
-        },
-      },
-      {
-        label: 'Payment',
-        command: (event: any) => {
-          this.activeIndex = 2;
-          console.log({ severity: 'info', summary: 'Pay with CC', detail: event.item.label });
-        },
-      },
-      {
-        label: 'Confirmation',
-        command: (event: any) => {
-          this.activeIndex = 3;
-          console.log({ severity: 'info', summary: 'Last Step', detail: event.item.label });
-        },
-      },
-    ];
+  ngOnInit(): void {}
+
+  updateSteps() {
+    if (!this._algorithmGroup) {
+      throw new Error('No algorithm group (traversal, apsp, spsp, sssp) was selected!');
+    }
+    if (!this._initializationInformation) {
+      throw new Error('No information about the initializatio process for the selected algorithm available!');
+    }
+
+    this.requiredSteps = this._initializationInformation.map((nodeInformation, index) => {
+      return {
+        label: nodeInformation.nodeName,
+        command: (event: any) => this.handleStepClick(index),
+        fullInformation: nodeInformation,
+      };
+    });
+    this.activeIndex = 0;
+    const firstStep = this.requiredSteps[0];
+    this.triggerUpdate(firstStep);
+  }
+
+  handleStepClick(index: number) {
+    const currentStep = this.requiredSteps[this.activeIndex];
+    this.activeIndex++;
+    this.triggerUpdate(currentStep);
+  }
+
+  triggerUpdate(newNodeSelection: MenuItem) {
+    this.updateCurrentNodeSelection.next({
+      //@ts-ignore
+      nodeType: newNodeSelection.fullInformation.nodeType,
+      //@ts-ignore
+      color: newNodeSelection.fullInformation.color,
+    });
   }
 }
