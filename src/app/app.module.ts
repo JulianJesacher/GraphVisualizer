710dd7b8543ac3d27d27779b4dcdf0347013f70c
Julian Jesacher <julianjesacher@gmail.com>
2022-08-27 10:28:53 +0200

    Add basic primeng step implementation for node selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { GraphComponent } from './components/graph-component/graph.component';
@@ -10,10 +11,18 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { NodeSelectorComponent } from './components/node-selector/node-selector.component';
 
 import { CardModule } from 'primeng/card';
+import { StepsModule } from 'primeng/steps';
 
 @NgModule({
   declarations: [AppComponent, GraphComponent, ToolbarComponent, GraphElementConfigComponent, NodeSelectorComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, ReactiveFormsModule, CardModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    CardModule,
+    StepsModule,
+    RouterModule.forRoot([{ path: '', component: AppComponent }]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/node-selector/node-selector.component.ts
+++ src/app/components/node-selector/node-selector.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-node-selector',
   templateUrl: './node-selector.component.html',
-  styleUrls: ['./node-selector.component.css']
+  styleUrls: ['./node-selector.component.css'],
 })
 export class NodeSelectorComponent implements OnInit {
+  public items: MenuItem[] = [];
+  activeIndex: number = 1;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Personal',
+        command: (event: any) => {
+          this.activeIndex = 0;
+          console.log({ severity: 'info', summary: 'First Step', detail: event.item.label });
+        },
+      },
+      {
+        label: 'Seat',
+        command: (event: any) => {
+          this.activeIndex = 1;
+          console.log({ severity: 'info', summary: 'Seat Selection', detail: event.item.label });
+        },
+      },
+      {
+        label: 'Payment',
+        command: (event: any) => {
+          this.activeIndex = 2;
+          console.log({ severity: 'info', summary: 'Pay with CC', detail: event.item.label });
+        },
+      },
+      {
+        label: 'Confirmation',
+        command: (event: any) => {
+          this.activeIndex = 3;
+          console.log({ severity: 'info', summary: 'Last Step', detail: event.item.label });
+        },
+      },
+    ];
   }
-
 }
