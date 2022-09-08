ff7d0e35f44257766798a938e64ec8c40a38bd04
Julian <julianjesacher@gmail.com>
2022-09-08 18:39:00 +0200

    Change design and add basic functionality to buttons

--- src/app/components/user-information-card/user-information-card.component.ts
+++ src/app/components/user-information-card/user-information-card.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { InformationCardData } from 'src/app/types/user-information-card.types';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { InformationCardData, InformationType } from 'src/app/types/user-information-card.types';
 import { tutorialCardInformation } from '../../config/tutorialConfig';
 
 @Component({
@@ -8,9 +8,45 @@ import { tutorialCardInformation } from '../../config/tutorialConfig';
   styleUrls: ['./user-information-card.component.css'],
 })
 export class UserInformationCardComponent implements OnInit {
-  @Input() currentInformation: InformationCardData = tutorialCardInformation[0];
+  public _currentInformation: InformationCardData[] = [tutorialCardInformation[0]];
+  @Input() set currentInformation(newInformation: InformationCardData[]) {
+    this._currentInformation = newInformation;
+    this._activeIndex = 0;
+    this._informationCardAmount = newInformation.length;
+    this._singleCard = newInformation.length === 1;
+  }
+
+  @Input() currentCardNumber: number = 1;
+  public _informationCardAmount: number = 2;
+  public _activeIndex: number = 0;
+  public _singleCard: boolean = true;
+
+  @Input() currentInformationType: InformationType = InformationType.TUTORIAL;
+  public informationTypes = InformationType;
+
+  @Output() closeCard = new EventEmitter<void>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  public nextCard(): void {
+    if (this._activeIndex >= this._informationCardAmount - 1) {
+      throw new Error('Already at the last card, can no go further!');
+    }
+
+    this._activeIndex++;
+  }
+
+  public previousCard(): void {
+    if (this._activeIndex <= 0) {
+      throw new Error('Already at the first card, can not go back!');
+    }
+
+    this._activeIndex--;
+  }
+
+  public triggerCloseCard(): void {
+    this.closeCard.next();
+  }
 }
